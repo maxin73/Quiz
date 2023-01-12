b527e6b4e1ec443fc596c81ff1c149a994e37da2
Nami Miyashiro <nami.miyashiro@gmail.com>
2023-01-12 12:00:18 -0800

    first commit

---
+++ Q1.js
@@ -0,0 +1,20 @@
+// Question 1:
+// Which value does x have after  
+// execution of the following code?
+// let x = "Pooh";
+// let y = "Tigger";
+// let z = y;
+// y = x;
+// x = z;
+
+
+let x = "Pooh";
+let y = "Tigger";
+let z = y;
+y = x;
+x = z;
+
+console.log(x);
+
+// Answer 1:
+// Tigger
